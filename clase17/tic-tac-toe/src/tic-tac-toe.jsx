87fd505bd12a4f264dde332093be16d780f00b3f
cosdealpha <bocangelcamila@gmail.com>
2026-08-27 18:08:45 -0400

    ticaca

---
+++ clase17/tic-tac-toe/src/tic-tac-toe.jsx
@@ -0,0 +1,21 @@
+export default function TicTacToe() {
+    return <section>
+        <table>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </section>
+}
